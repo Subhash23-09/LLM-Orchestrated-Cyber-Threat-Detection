823beaa9bd52551721f7b36de213a158d9612323
Subhash <sj7766@srmist.edu.in>
2026-05-24 20:10:01 +0530

    Remove generate_ddos_log.py

--- generate_ddos_log.py
+++
@@ -1,3 +0,0 @@
-with open('ddos_simulation.log', 'w') as f:
-    for i in range(1, 1501):
-        f.write(f"Jan 04 16:00:01 web-server-01 custom_app[1234]: UNKNOWN_FLOOD_EVENT from 45.33.22.11 - seq={i}\n")
